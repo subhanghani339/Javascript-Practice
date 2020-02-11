4328e6e1575d327020059b0851127a0d77f9ab6f
subhanghani339 <subhanghani339@gmail.com>
2020-02-11 20:54:47 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var date = new Date();
+var newYear = new Date("January 2,2020")
+
+var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+var day_index = date.getDay();
+
+console.log(date.getDay())
+console.log(date.getMonth())
+console.log(date.getFullYear())
+console.log(date.getHours())
+console.log(date.getMinutes())
+console.log(date.getSeconds())
+console.log(date.getMilliseconds())
+console.log(date.getTime())
+// console.log(days[day_index])
+console.log("---------------------------------------------------------")
+console.log(newYear.getDay())
+console.log(newYear.getMonth())
+console.log(newYear.getFullYear())
+console.log(newYear.getHours())
+console.log(newYear.getMinutes())
+console.log(newYear.getSeconds())
+console.log(newYear.getMilliseconds())
+console.log(newYear.getTime())
